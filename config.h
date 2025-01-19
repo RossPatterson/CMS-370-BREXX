a0b88a2f61ac98ae250bc1608d0d028e00a686b1
Ross Patterson <ross.patterson@gmail.com>
2025-01-19 14:32:42 -0500

    Update version number.

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* Modified for VM/370 CMS and GCC by Robert O'Hara, July 2010. */
 
-/* The one version to rule them all! */
-#define CMS_VERSION "1.1.0-beta-03"
+#define CMS_VERSION "1.1.0"
+
 /*
 #define __DEBUG__
 */
