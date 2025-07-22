2bacbf27d209437c6c5f187fae6fb727050c86b7
Ross Patterson <ross.patterson@gmail.com>
2025-07-22 18:23:37 -0400

    I0088 - GitHub build occasionally fails to compile CHANGEST C.  Fix #88.

--- cmsbuild.sh
+++ cmsbuild.sh
@@ -82,6 +82,9 @@ herccontrol "/COPYFILE * COPY     B (RECFM F LRECL 80" -w "^Ready"
 herccontrol "/COPYFILE * ASSEMBLE B (RECFM F LRECL 80" -w "^Ready"
 herccontrol "/RENAME NEWBREXX TMPFTYPE B NEWBREXX CONTROL B" -w "^Ready"
 
+# TEMPORARY!  Build GCCCSECT MODULE, until a new VM/CE release ships our version.
+herccontrol "/MKGCCCS" -w "^Ready;"
+
 # Make the BRXHELP VMARC file
 herccontrol "/EXPLOIT (BRXHELP) VMARC PACK &FN &FT B BRXHELP VMARC B (APPEND NOTRACE" -w "^Ready;"
 
